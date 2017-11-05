fe6e45a0b3698ac003eb8b10be55b0546fcd103b
Hower16 <miky1216@gmail.com>
2017-11-05 11:35:18 -0600

    Added check if book does not exist yet in BookDetail

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
-export default function(){
+export default function() {
   return [
-    {title: 'JavaScript: The Good Parts'},
-    {title: 'Harry Potter and the Goblet of Fire'},
-    {title: 'The Dark Tower'},
-    {title: 'Eloquent Ruby'}
-  ]
+    { title: "Javascript: The Good Parts", pages: 101 },
+    { title: "Harry Potter", pages: 39 },
+    { title: "The Dark Tower", pages: 85 },
+    { title: "Eloquent Ruby", pages: 1 }
+  ];
 }
